56992e06b9069a87154c441a4541af914393ea0f
George <sandovalcastrejon@gmail.com>
2024-12-15 00:25:16 -0500

    feat(): fix payment

--- src/main/java/booking/framework/pages/PaymentPage.java
+++ src/main/java/booking/framework/pages/PaymentPage.java
@@ -15,6 +15,7 @@ public class PaymentPage extends BaseMobile {
         waitvisibility(Duration.ofSeconds(10), PaymentObject.txtCardNumber);
         type("455578876544333",PaymentObject.txtCardNumber);
         type("02/25",PaymentObject.txtFechaExpiracion);
-        waitClickable(Duration.ofSeconds(5),PaymentObject.btnpayment);
+
+        waitvisibility(Duration.ofSeconds(5),PaymentObject.btnpayment);
     }
 }
